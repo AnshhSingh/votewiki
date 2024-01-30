cdc24f33c3c08ac509d8aa6db3eb85bdf7e710bf
anaypurohit0907 <ap1851@srmist.edu.in>
2024-01-30 19:52:31 +0530

    added some wiki api things

--- api/index.js
+++
@@ -1,7 +0,0 @@
-fetch(
-  "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Craig%20Noone&format=json"
-).then((res) => {
-  res.parse().then((data) => {
-    console.log(data);
-  });
-});
